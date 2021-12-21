84dd8b1ef205f80708657b8cc2ee510af14b52a5
Gianfranco De Paula <baciliogianfranco@gmail.com>
2021-12-21 12:57:51 -0400

    Agregando cambios en la base de datos, ademas de templates en posts

--- platzigram/posts/models.py
+++ platzigram/posts/models.py
@@ -1,3 +1,23 @@
+#Importaciones de Django
 from django.db import models
+from django.db.models.fields import EmailField, TextField
 
 # Create your models here.
+
+#Model usuarios
+class User(models.Model):
+
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=30)
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    bio = models.TextField(blank=True)
+
+    is_admin = models.BooleanField(default=False)
+
+    birthdate = models.DateField(blank=True, null=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
--- platzigram/posts/views.py
+++ platzigram/posts/views.py
@@ -2,45 +2,43 @@
 
 #Importaciones de Django
 from django.shortcuts import render
-from django.http import HttpResponse
 
 #Utilities
 from datetime import datetime as dt
 
 #Globales
+
 posts_list_dictionaries = [
     {
-        'name': 'Gianfranco De Paula',
-        'user': 'jr.gian_k',
-        'timestamp': dt.now().strftime('%d/%m/%Y, %H:%M hrs'),
-        'picture': 'https://picsum.photos/id/237/200/200',
+        'title': 'Mont Blanc',
+        'user': {
+            'name': 'Yésica Cortés',
+            'picture': 'https://picsum.photos/60/60/?image=1027'
+        },
+        'timestamp': dt.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/800/600?image=1036',
     },
-
     {
-        'name': 'Khe.',
-        'user': 'Pink Woman',
-        'timestamp': dt.now().strftime('%d/%m/%Y, %H:%M hrs'),
-        'picture': 'https://picsum.photos/id/84/200/200'
+        'title': 'Via Láctea',
+        'user': {
+            'name': 'Christian Van der Henst',
+            'picture': 'https://picsum.photos/60/60/?image=1005'
+        },
+        'timestamp': dt.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/800/800/?image=903',
     },
-
     {
-        'name': 'Nautural web.',
-        'user': 'Pancho Villa',
-        'timestamp': dt.now().strftime('%d/%m/%Y, %H:%M hrs'),
-        'picture': 'https://picsum.photos/id/784/200/200'
-    },
+        'title': 'Nuevo auditorio',
+        'user': {
+            'name': 'Uriel (thespianartist)',
+            'picture': 'https://picsum.photos/60/60/?image=883'
+        },
+        'timestamp': dt.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/500/700/?image=1076',
+    }
 ]
 
 # Create your views here.
 def list_post(request):
-    
-    const = []
 
-    for post in posts_list_dictionaries:
-        const.append("""
-        <p><strong>{name}</strong></p>
-        <p><small>{user} - <i>{timestamp}</i></small></p>
-        <figure><img src = "{picture}"></figure>
-        """.format(**post))
-
-    return HttpResponse("<br>".join(const))
+    return render(request, 'feed.html', {'posts': posts_list_dictionaries})
